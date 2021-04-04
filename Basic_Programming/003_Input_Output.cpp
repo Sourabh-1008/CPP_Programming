3003be7f45d3eaeda4d957574b7c6dcdf86a4204
Sourabh-1008 <sourabhhawle108@gmail.com>
2021-04-04 15:18:24 +0530

    commit

---
+++ Basic_Programming/003_Input_Output.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int amount;
+    cout<<"Enter the Amount:-";
+    cin>>amount;
+    
+    int amount2;
+    cout<<"Enter the second amount:-";
+
+    cin>>amount2;
+
+    int sum =amount + amount2;
+    cout<<"Total:-"<<sum;
+
+}

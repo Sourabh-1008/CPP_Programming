af20e2d62162ca0d144602c965394c823610a013
Sourabh-1008 <sourabhhawle108@gmail.com>
2021-04-04 15:04:24 +0530

    commit

---
+++ Basic_Programming/002_datatypes.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x; //declaration
+    x =30; //initialisation
+    int a = 1;
+    float b = 23.2;
+    char c;
+    bool d;
+    cout<<"Datatype of Int"<<" "<<sizeof(a)<<endl;
+    cout<<"Datatype of Float"<<" "<<sizeof(b)<<endl;
+    cout<<"Datatype of Char"<<" "<<sizeof(c)<<endl;
+    cout<<"Datatype of Bool"<<" "<<sizeof(d)<<endl;
+
+    long long  s = 71;
+    short r = 230;
+    cout<<"Datatype of Long"<<" "<<sizeof(s)<<endl;
+    cout<<"Datatype of Short"<<" "<<sizeof(r)<<endl;
+
+    
+    
+}
